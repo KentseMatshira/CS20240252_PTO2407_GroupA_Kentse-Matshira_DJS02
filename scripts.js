139849de5b5b60168fee7370cb25d6c28427b61a
KentseMatshira <kmatshira@yahoo.com>
2025-03-23 19:29:05 +0200

    fix to try catch error

--- scripts.js
+++ scripts.js
@@ -3,15 +3,43 @@ const result = document.querySelector("[data-result]");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  const entries = new FormData(event.target);
-  const { dividend, divider } = Object.fromEntries(entries);
-  if (!dividend || !divider) {
-    result.innerText =
-      "Division not performed. Both values are required in inputs. Try again.";
-  } else if (dividend > divider) {
-     result.innerText = 'Division not performed. Invalid number provided. Try again.'
-     console.error('Invalid Numbers Entered');
+  try {
+    const entries = new FormData(event.target);
+    const { dividend, divider } = Object.fromEntries(entries);
+
+    if (!dividend || !divider) {
+      result.innerText =
+        "Division not performed. Both values are required in inputs. Try again.";
+    } else if (isNaN(dividend) || isNaN(divider)) {
+      result.innerText =
+        "Something critical went wrong. Please reload the page.";
+      throw new Error("Critical Error: NaN Provided");
+    } else if (dividend > divider) {
+      result.innerText =
+        "Division not performed. Invalid number provided. Try again.";
+      console.error("Invalid Numbers Entered");
+    } else {
+      result.innerText = Math.floor(dividend / divider);
+    }
+  } catch (error) {
+    console.error(error.stack);
+  }
+});try {
+     const entries = new FormData(event.target);
+     const { dividend, divider } = Object.fromEntries(entries);
+ 
+     if (!dividend || !divider) {
+       result.innerText = 'Division not performed. Both values are required in inputs. Try again.'
      } else if (isNaN(dividend) || isNaN(divider)) {
-     result.innerText = 'Something critical went wrong. Please reload the page.';
-     throw new Error('Critical Error: NaN Provided');
-});
+       result.innerText = 'Something critical went wrong. Please reload the page.';
+       throw new Error('Critical Error: NaN Provided');
+     } else if (dividend > divider) {
+       result.innerText = 'Division not performed. Invalid number provided. Try again.'
+       console.error('Invalid Numbers Entered');
+     } else {
+       result.innerText = Math.floor(dividend / divider);
+     }
+   } catch (error) {
+     console.error(error.stack)
+   }
+ });
