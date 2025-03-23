14f2d575494b6334421149af70786448dc56d2ef
KentseMatshira <kmatshira@yahoo.com>
2025-03-23 19:16:23 +0200

    fix decimal number to floor

--- scripts.js
+++ scripts.js
@@ -6,4 +6,5 @@ form.addEventListener("submit", (event) => {
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
   result.innerText = dividend / divider;
+  result.innerText = Math.floor(dividend / divider);
 });
